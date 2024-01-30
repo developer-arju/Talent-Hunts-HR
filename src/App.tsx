cca70b9d6e8a54140213096895173d510a3fe258
developer-arju <arjunsajeevan369@gmail.com>
2024-01-30 17:29:41 +0530

    template ready

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="wrapper">
+      <p>Talent Hunts HR</p>
+      <h1>work in progress</h1>
+      <div>
+        <p>out exciting new website wil launch soon</p>
+        <p>come back and check it out</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
